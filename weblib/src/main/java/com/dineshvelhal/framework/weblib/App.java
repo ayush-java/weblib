eacef6cfa16835536cbacf9bba86b3bcbac9ff2c
Dinesh Velhal <dinesh.velhal@gmail.com>
2019-02-22 23:33:38 +0530

    100 project create

---
+++ weblib/src/main/java/com/dineshvelhal/framework/weblib/App.java
@@ -0,0 +1,13 @@
+package com.dineshvelhal.framework.weblib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
