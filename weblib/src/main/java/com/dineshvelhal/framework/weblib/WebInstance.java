a2d2245c8ddfe2791833ee4f9b28cb34541e878a
Dinesh Velhal <dinesh.velhal@gmail.com>
2019-02-23 07:24:17 +0530

    Classes created

---
+++ weblib/src/main/java/com/dineshvelhal/framework/weblib/WebInstance.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.dineshvelhal.framework.weblib;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author Dinesh_Velhal
+ *
+ */
+@Log4j2
+public class WebInstance {
+
+}
---
+++ weblib/src/test/java/com/dineshvelhal/framework/weblib/WebInstanceTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.dineshvelhal.framework.weblib;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author Dinesh_Velhal
+ *
+ */
+public class WebInstanceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue("Dummy Test", 1 == 1);
+	}
+
+}
