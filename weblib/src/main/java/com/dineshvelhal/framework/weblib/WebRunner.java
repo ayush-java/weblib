c45c8b237ff9325608540fb28c195ef7b311a4aa
Dinesh Velhal <dinesh.velhal@gmail.com>
2019-02-26 02:54:02 +0530

    Removed unused imports

--- weblib/src/main/java/com/dineshvelhal/framework/weblib/WebRunner.java
+++ weblib/src/main/java/com/dineshvelhal/framework/weblib/WebRunner.java
@@ -4,14 +4,13 @@
 package com.dineshvelhal.framework.weblib;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.log4j.Log4j2; 
 
 /**
  * @author Dinesh_Velhal
