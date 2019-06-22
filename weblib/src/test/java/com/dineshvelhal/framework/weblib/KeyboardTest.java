22857831566c3f8b67df6d02b70df87df2400be1
dinesh <dinesh.velhal@gmail.com>
2019-06-23 02:50:51 +0530

    More Keyboard Tests added. keyboard_events.html updated.

--- weblib/src/test/java/com/dineshvelhal/framework/weblib/KeyboardTest.java
+++ weblib/src/test/java/com/dineshvelhal/framework/weblib/KeyboardTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import lombok.extern.log4j.Log4j2;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.io.File;
@@ -51,14 +52,33 @@ public class KeyboardTest {
 	}
 
 	@Test
-	public void specialKeyTests() {
+	public void modifierKeyTest() {
 		runner.sendKeys(areaLoc, "Dinesh");
 		runner.sendKeys(By.id("area"), Keys.CONTROL, "a");
 		assertTrue(runner.smartFindElement(By.id("ctrl")).isDisplayed(), "Ctrl key press was not detected");
 
-		runner.sendKeys(By.id("area"), Keys.CONTROL, Keys.ALT, Keys.SHIFT, "a");
+		runner.sendKeys(By.id("area"), Keys.CONTROL, Keys.ALT, Keys.SHIFT, Keys.META, "a");
 		assertTrue(runner.smartFindElement(By.id("ctrl")).isDisplayed() 
 				&& runner.smartFindElement(By.id("alt")).isDisplayed() 
-				&& runner.smartFindElement(By.id("shift")).isDisplayed(), "Ctrl, Alt or Shift keypress was not detected");
+				&& runner.smartFindElement(By.id("shift")).isDisplayed()
+				&& runner.smartFindElement(By.id("meta")).isDisplayed(), "Ctrl, Alt, Meta or Shift keypress was not detected");
+	}
+	
+	@Test
+	public void specialKeyPressTest() {
+		assertEquals(runner
+				.sendKeys(By.id("area"), Keys.ENTER)
+				.smartFindElement(By.id("key"))
+				.getText(), "Enter", "Enter Key Press was not detected");
+		
+		assertEquals(runner
+				.sendKeys(By.id("area"), Keys.PAGE_DOWN)
+				.smartFindElement(By.id("key"))
+				.getText(), "PageDown", "PageDown Key Press was not detected");
+		
+		assertEquals(runner
+				.sendKeys(By.id("area"), "d")
+				.smartFindElement(By.id("code"))
+				.getText(), "KeyD", "Key d Press was not detected");
 	}
 }
